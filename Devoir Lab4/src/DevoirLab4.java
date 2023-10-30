b050ff0405862c91f82ce947d3c8c7a3556f70d4
cataratoru-fara-cap <gabivaireanu@gmail.com>
2023-10-31 00:45:21 +0200

    Solved ex 3 and 4 and fixed a new  error from ex 2

--- Devoir Lab4/src/DevoirLab4.java
+++ Devoir Lab4/src/DevoirLab4.java
@@ -26,7 +26,7 @@ public class DevoirLab4 {
                 break;
         }
     }
-    //ex2 Inputer
+    //ex2 a)  Inputer
     public static double[][] inputer(int N){
         double[][] table = new double[N][N];
         try (Scanner input = new Scanner(System.in)) {
@@ -40,12 +40,12 @@ public class DevoirLab4 {
         return table;
    }
 }
-
+    //ex2 c)
     public static double multiplyEvenHalf(double[][] args){
         double product = 1;
         int modifiableLength = args.length;
         for(int i = 0; i < args.length; i++){
-            for (int j = 0; j <= modifiableLength; j++){
+            for (int j = 0; j < modifiableLength; j++){
                 if (args[i][j] % 2 == 0) {
                     product *=  args[i][j];
                 }
@@ -59,12 +59,13 @@ public class DevoirLab4 {
         double product = 1;
         int start = 0;
         for(int i = 0; i < args.length; i++){
-            start++;
-            for (int j = start; j <= args.length - start; j++){
+            for (int j = start; j < args.length - start; j++){
                 if (args[i][j] % 2 == 0) {
                     product *=  args[i][j];
                 }
             if (start == args.length / 2) {break;}
+            start++;
+
             }
         }
     return product;
@@ -74,7 +75,7 @@ public class DevoirLab4 {
         double sum = 0;
         int modifiableLength = args.length;
         for(int i = 0; i < args.length; i++){
-            for (int j = 0; j <= modifiableLength; j++){
+            for (int j = 0; j < modifiableLength; j++){
                 if (args[i][j] > 0) {
                     sum +=  Math.pow(args[i][j], 2);
                 }
@@ -88,27 +89,78 @@ public class DevoirLab4 {
         double sum = 0;
         int start = 0;
         for(int i = 0; i < args.length; i++){
-            start++;
-            for (int j = start; j <= args.length - start; j++){
+            for (int j = start; j < args.length - start; j++){
                 if (args[i][j] > 0) {
                     sum +=  Math.pow(args[i][j], 2);
+                    System.out.println(sum);
                 }
             if (start == args.length / 2) {break;}
+            start++;
             }
         }
     return sum;
     }
+    //ex3 and ex4
+    public static double [][] matrixpow(double [][] args1, double [][] args2){
+        double [][] c = new double[args1.length][args1.length];
+        for( int i = 0; i < args1.length; i++ ){
+            for( int j = 0; j < args1.length; j++){
+                for (int k = 0; k < args1.length; k++)
+                    c[i][j] += args1[i][k] * args2[k][j];
+            }
+        }
+        return c;
+    }
+    
+    public static double [][] matrixsum(double [][] args1, double [][] args2){
+        double [][] C = new double[args1.length][args1.length];
+        for (int i = 0; i < args1.length; i ++){
+            for(int j = 0; j < args1.length; j++)
+                C[i][j] += args1[i][j] + args2[i][j];
+        }
+        return C;
+    }
+    
+    public static double [][] matrixsum(double [][] args1, double [][] args2, double [][] args3){
+        double [][] C = new double[args1.length][args1.length];
+        for (int i = 0; i < args1.length; i ++){
+            for(int j = 0; j < args1.length; j++)
+                C[i][j] += args1[i][j] + args2[i][j] + args3[i][j];
+        }
+        return C;
+    }
+   
+    public static double [][] matrixtranspose(double [][] args){
+        double [][] c = new double[args.length][args.length];
+        for (int i = 0; i < args.length; i ++){
+            for(int j = 0; j < args.length; j++)
+                c[j][i] += args[i][j];
+        }
+        return c;
+    }
     public static void main(String[] args) {
         //caser(8);
-        double [][] table = inputer(2);
+        double [][] table = inputer(3);
+        //ex 2 b)
         //System.out.println(Arrays.deepToString(table));
+        
+        //ex 2 c)
         //double a = multiplyEvenHalf(table);
         //double b = multiplyEvenFourth(table);
         //double c = sumPosHalf(table);
+        //double d = sumPosFourth(table);
 
         //System.out.println(a);
         //System.out.println(b);
         //System.out.println(c);
+        //System.out.println(d);
+
+        //ex 3 and 4
+        //double [][] tableSecond = matrixpow(table, table);
+        //double [][] tableThird = matrixpow(tableSecond, table);
+        //double [][] C = matrixsum(table, tableSecond, tableThird);
+        //double [][] transpC = matrixtranspose(C);  
+        //System.out.println(Arrays.deepToString(transpC));
 
 
     }
