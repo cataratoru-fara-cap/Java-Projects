e1d0df4bd4a3f4ffaf94857878a29a75110916ad
cataratoru-fara-cap <gabivaireanu@gmail.com>
2023-11-22 01:05:01 +0200

    Wrote first ex and added docs

---
+++ LabHw/Devoir Lab7/src/Ex1.java
@@ -0,0 +1,9 @@
+public class Ex1 {
+    public static void main(String[] args) {
+        int a = 2;
+        String b = "3";
+        int c = 5;
+        int d = 7;
+        System.out.println(a+b+c+d);
+    }
+}
