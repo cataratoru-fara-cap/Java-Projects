5d6d62b0ff74363975da1137471229c9a9c2e092
cataratoru-fara-cap <gabivaireanu@gmail.com>
2023-11-29 11:51:24 +0200

    Added two new methods and created test class

--- LabEx/Lab9Ex/src/Auteur.java
+++ LabEx/Lab9Ex/src/Auteur.java
@@ -4,14 +4,14 @@ public class Auteur {
     private int code;
     private String nom;
     private String prenom;
-    private int age;
+    private String pays;
     private static int counter = 0;
     public ArrayList<Livre> liste = new ArrayList<>();
     
-    public Auteur(String nom, String prenom, int age){
+    public Auteur(String nom, String prenom, String pays){
         this.nom = nom;
         this.prenom = prenom;
-        this.age = age;
+        this.pays = pays;
         counter++;
         code = counter;
     }
@@ -23,7 +23,7 @@ public class Auteur {
     }
     @Override
     public String toString(){
-        return this.nom + " " + this.prenom + " " + this.age + " " + this.code;
+        return this.nom + " " + this.prenom + " " + this.pays + " " + this.code;
     }
     
     public void addLivre (Livre livre) {
@@ -34,9 +34,29 @@ public class Auteur {
             System.out.println( liste.get(i) );
         }
     }
+    public boolean modifierDomaine(String domaine, String ISBN) {
+        for (int i = 0; i < liste.size(); i++) {
+            if ( (liste.get(i).getDomaine().equals(ISBN)) ) {
+                liste.get(i).setDomaine(domaine);
+                return true;
+            }
+        }
+        return false;
+    }
+    public Livre mostPages(){
+        int max = 0;
+        Livre maxLivre = liste.get(0);
+        for (Livre book : liste) {
+            if (book.getNumPages() >= max) {
+                maxLivre = book;
+                max = book.getNumPages();
+            }
+        }
+        return maxLivre;
+    }
 
     public static void main(String[] args) throws Exception {
-        Auteur Gabi = new Auteur(null, null, counter);
+        Auteur Gabi = new Auteur(null, null, null);
         System.out.println(Gabi.toString());
     }
 }
