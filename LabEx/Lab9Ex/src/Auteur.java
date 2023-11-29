b207629f68b9e3d671350902dc29caee6620fcbe
cataratoru-fara-cap <gabivaireanu@gmail.com>
2023-11-29 11:22:47 +0200

    Added getters and setters to Auteur and LIvre

--- LabEx/Lab9Ex/src/Auteur.java
+++ LabEx/Lab9Ex/src/Auteur.java
@@ -1,9 +1,12 @@
+import java.util.ArrayList;
+
 public class Auteur {
     private int code;
     private String nom;
     private String prenom;
     private int age;
     private static int counter = 0;
+    public ArrayList<Livre> liste = new ArrayList<>();
     
     public Auteur(String nom, String prenom, int age){
         this.nom = nom;
@@ -23,6 +26,15 @@ public class Auteur {
         return this.nom + " " + this.prenom + " " + this.age + " " + this.code;
     }
     
+    public void addLivre (Livre livre) {
+        liste.add(livre);
+    }
+    public void printLivres(){
+        for (int i = 0; i < liste.size(); i++) {
+            System.out.println( liste.get(i) );
+        }
+    }
+
     public static void main(String[] args) throws Exception {
         Auteur Gabi = new Auteur(null, null, counter);
         System.out.println(Gabi.toString());
