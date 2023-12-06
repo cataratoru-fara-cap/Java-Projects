f5ccf4e36f82fdc08da5ac90e86c0c612a8a1ee1
cataratoru-fara-cap <gabivaireanu@gmail.com>
2023-12-07 00:43:34 +0200

    Finished LabHw

--- LabHw/Devoir Lab9/src/Item.java
+++ LabHw/Devoir Lab9/src/Item.java
@@ -32,11 +32,4 @@ public class Item {
             return new String(nom) + " " + id + " " + prix;
         return new String(nom) + " " + description + " " + id + " " + prix;
     }
-
-    public static void main(String[] args) throws Exception {
-        Item i1 = new Item("Vodka", "Une bouteille de Vodka", 5483918746738L, 75.5);
-        Item i3 = new Item("Fillet de citron 200g", 5473664615361L, 3.25);
-        System.out.println(i1.toString());
-        System.out.println(i3.toString());
-    }
 }
