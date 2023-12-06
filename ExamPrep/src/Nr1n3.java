856048d60ffa364db4e0ef57de0dd4b22b6046e0
cataratoru-fara-cap <gabivaireanu@gmail.com>
2023-12-06 23:27:12 +0200

    Finished ex 1

--- ExamPrep/src/Nr1n3.java
+++ ExamPrep/src/Nr1n3.java
@@ -1,7 +1,5 @@
-import javax.swing.plaf.TreeUI;
 
 public class Nr1n3 {
-    private static final String Error = null;
     public static String reverseString(String str) {
         char[] charArray = str.toCharArray();
         String n_String = "";
@@ -34,7 +32,7 @@ public class Nr1n3 {
         int sum = 0;
         for (int i = 0; i < arr.length; i++)
             for (int j = 0; j < arr.length; j++) 
-                if ((arr.length / 2 < i) && (i+j))
+                if ((arr.length / 2 < i) && ( arr.length < i+j))
                     sum += arr[i][j];
         return sum;
     }
--- LabHw/Devoir Lab9/src/Item.java
+++ LabHw/Devoir Lab9/src/Item.java
@@ -1,4 +1,42 @@
 public class Item {
+    private char[] nom;
+    private String description;
+    private double prix;
+    private long id;
+
+    public Item(String nom, String description, long id, double prix) {
+        this.nom = nom.toCharArray();
+        this.description = description;
+        this.id = id;
+        this.prix = prix;
+    }
+
+    public Item(String nom, long id, double prix) {
+        this.nom = nom.toCharArray();
+        this.id = id;
+        this.prix = prix;
+    }
+    
+    public double getPrix() {
+        return prix;
+    }
+
+    public double getPrix(int q) {
+        if (q >= 5)
+            return prix * 0.95;
+        return prix;
+    }
+    
+    public String toString() {
+        if (description == null)
+            return new String(nom) + " " + id + " " + prix;
+        return new String(nom) + " " + description + " " + id + " " + prix;
+    }
+
     public static void main(String[] args) throws Exception {
+        Item i1 = new Item("Vodka", "Une bouteille de Vodka", 5483918746738L, 75.5);
+        Item i3 = new Item("Fillet de citron 200g", 5473664615361L, 3.25);
+        System.out.println(i1.toString());
+        System.out.println(i3.toString());
     }
 }
