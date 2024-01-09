127490a6312154ae88b14619feb1fc542932ffa9
cataratoru-fara-cap <gabivaireanu@gmail.com>
2024-01-10 01:54:18 +0200

    Created Loan Class template

---
+++ LabHw/Devoir Lab11/src/Loan.java
@@ -0,0 +1,55 @@
+public class Loan {
+    private int id;
+    private int number;
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String frequency;
+    private double sum;
+    private double instalment;
+    private double unpaidSum;
+
+    public Loan(String firstName, String lastName, String address, double sum) {
+        firstName = this.firstName;
+        lastName = this.lastName;
+        address = this.address;
+        sum = this.sum;
+    }
+    
+    public Loan(String firstName, String lastName, String address, String frequency, double sum) {
+        firstName = this.firstName;
+        lastName = this.lastName;
+        address = this.address;
+        frequency = this.frequency;
+        sum = this.sum;
+    }
+
+    public double getUnpaidSum() {
+        return this.unpaidSum;
+    }
+
+    public double getSum(){
+        return this.sum;
+    }
+
+    public void setUnpaidSum(double unpaidSum) {
+        this.unpaidSum = unpaidSum;
+    }
+
+    public void computeInstalment() {
+        this.getSum() / 
+    }
+    
+    public void payInstalment() {
+
+    }
+
+    @Override
+    public String toString() {
+        return "Loan [id=" + id + ", number=" + number + ", firstName=" + firstName + ", lastName=" + lastName
+                + ", address=" + address + ", frequency=" + frequency + ", sum=" + sum + ", instalment=" + instalment
+                + ", unpaidSum=" + unpaidSum + "]";
+    }
+    
+}
+
