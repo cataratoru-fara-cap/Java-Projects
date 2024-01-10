bf81eb6d8beedaaa9073a281b89faf7aece8dc60
cataratoru-fara-cap <gabivaireanu@gmail.com>
2024-01-10 11:32:36 +0200

    Started work on lab12

---
+++ LabEx/Lab12Ex/src/Client.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.GregorianCalendar;
+
+public class Client{
+    private String nom;
+    private GregorianCalendar aniversaire;
+    private String cnp;
+    Adresse adresse;
+    ArrayList<Carte> cartes = new ArrayList<>();
+    
+    public Client(String nom, GregorianCalendar aniversaire, String cnp, String addresse){
+         this.nom = nom;
+         this.aniversaire = aniversaire;
+         this.cnp = cnp;
+         this.adresse = addresse;
+    }
+
+    public boolean ajouterCarte(Carte c){
+        return cartes.add(c);
+    }
+
+    public Carte supprimerCarte (String iban){
+        Carte returnable = null;
+        for (Carte c : cartes) {
+            if (c.getIban.equals(iban)) {
+                returnable = c;
+                cartes.remove(c);
+            }
+            break;
+        }
+        return returnable;
+    }
+}
