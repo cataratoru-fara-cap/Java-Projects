c624fa14a45b5e326041912c460dfbc58c7a757f
cataratoru-fara-cap <gabivaireanu@gmail.com>
2024-01-16 17:50:05 +0200

    Created sub-classes and modified one of the parent methods

---
+++ LabHw/Lab12Hw/src/Moniteur.java
@@ -0,0 +1,18 @@
+public class Moniteur extends HardwareProduct {
+    private long money;
+    private int maximumNote;
+
+    public Moniteur(double monnaiePrix, double note){
+        this.setMonnaiePrix(monnaiePrix);
+        this.setNote(note);
+    }
+
+    public void calculerPrixLei(){
+
+    }
+
+    public void calculerPerformance(){
+
+    }
+
+}
---
+++ LabHw/Lab12Hw/src/VideoCard.java
@@ -0,0 +1,17 @@
+public class VideoCard extends HardwareProduit{
+    private long monnaie;
+    private int maximumNote;
+
+    public VideoCard(double monnaiePrix, double note){
+        this.setMonnaiePrix(monnaiePrix);
+        this.setNote(note);
+    }
+
+    public void calculerPrixLei(){
+
+    }
+
+    public void calculerPerformance(){
+
+    }
+}
