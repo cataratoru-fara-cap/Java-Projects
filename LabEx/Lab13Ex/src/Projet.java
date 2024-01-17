e72b6b66bb4c13d61a049a6fe536a940facd2243
cataratoru-fara-cap <gabivaireanu@gmail.com>
2024-01-17 10:37:04 +0200

    Started work on lab 13

---
+++ LabEx/Lab13Ex/src/Projet.java
@@ -0,0 +1,24 @@
+public class Projet {
+    private String nom;
+    private int buget;
+
+    public Projet(String nom, int buget){
+        this.nom = nom;
+        this.buget = buget;
+    }
+
+    public int getBuget(){
+        return this.buget;
+    }
+
+    public void setBuget(int buget){
+        this.buget = buget;
+    }
+    public String getNom(){
+        return this.nom;
+    }
+
+    public void setNom(String nom){
+        this.nom = nom;
+    }
+}
